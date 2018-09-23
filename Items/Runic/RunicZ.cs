9caa893396b3ca24e06bfbcb1213cfa7f6ca6820
sjcircle95 <43523822+sjcircle95@users.noreply.github.com>
2018-09-23 13:33:10 -0700

    Add files via upload

---
+++ Items/Runic/RunicZ.cs
@@ -0,0 +1,35 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AuroraMod.Items.Runic
+{
+    public class RunicZ : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+			DisplayName.SetDefault("Runic Letter");
+            Tooltip.SetDefault("A rune written in stone that might be a letter.\nYou're not sure how you made it, you just knew how...\nPossibly translates to "Z"");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.maxStack = 999;
+            item.rare = 1;
+
+        }
+
+        public override void AddRecipes()
+        {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(mod.GetItem("RuneInscriber"), 1);
+			recipe.AddIngredient(ItemID.Stone, 26);
+			recipe.AddTile(TileID.Anvil);
+			//recipe.AddTile(TileID.RunicInscriber);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+        }
+    }
+}
